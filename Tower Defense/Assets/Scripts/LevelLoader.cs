a8a3778615d5ef2989a958fc2e8205affcf5da31
thomz12 <thomz12@hotmail.nl>
2015-09-14 23:26:52 +0200

    Started level loader
    idea: have levels load from textues; you can create your own, or
    randomly generate one.

---
+++ Tower Defense/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+    void Start()
+    {
+        //placeholder texture
+        Texture2D level = new Texture2D(6,6);
+        for (int i = 0; i < level.height; i++)
+            for (int j = 0; j < level.width; j++)
+                level.SetPixel(i, j, Color.white);
+
+        //load the level with placeholder texture
+        LoadLevel(level);
+    }
+
+    public void LoadLevel(Texture2D levelLayout)
+    {
+        int width = levelLayout.width;
+        int height = levelLayout.height;
+
+        for(int y = -height / 2; y <= height / 2; y++)
+        {
+            for(int x = -width / 2; x <= width / 2; x++)
+            {
+                Debug.Log(x + "," + y);
+            }
+        }
+    }
+
+    //TODO
+    public void DestoryLevel()
+    {
+
+    }
+}
